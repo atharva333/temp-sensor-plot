420254624ea9711e8ad6940248c988c2c385ecad
Atharva Deshmukh <atharvad@gmail.com>
2023-02-11 19:55:52 +0000

    Added get request for nest thermostat

--- test_live.py
+++ test_live.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import board
 import adafruit_dht
 
+import requests
+import json
+
 import dash
 from dash import dcc, html
 import plotly
@@ -50,11 +53,59 @@ class DHTSensorData:
         self.dhtDevice.exit()
         return None
 
+class NestAPIData:
+
+    api_config_file = "api_token.json"
+
+    def __init__(self):
+
+        with open(self.api_config_file) as fileread:
+            self.api_info = json.load(fileread)
+
+        self.access_token = self.get_access_token()
+
+        self.temperature_datapoints = []
+        self.humidity_datapoints = []
+
+    def get_access_token(self):
+        data = {
+            "client_id": self.api_info["client_id"],
+            "client_secret": self.api_info["client_secret"],
+            "refresh_token": self.api_info["refresh_token"],
+            "grant_type":"refresh_token",
+            }
+        url = "https://www.googleapis.com/oauth2/v4/token?"
+
+        response = requests.post(url, data)
+
+        return response.json()["access_token"]
+
+    def get_nest_sensor_data(self):
+
+        headers = {
+            "Content-Type": "application/json", 
+            "Authorization": f"Bearer {self.access_token}"}
+
+        url = "https://smartdevicemanagement.googleapis.com/v1/enterprises/3f7b67ed-ad48-43d0-b6cf-9b05132cee6b/devices"
+
+        response = requests.get(url, headers=headers)
+
+        temperature = response.json()["devices"][0]["traits"]["sdm.devices.traits.Temperature"]["ambientTemperatureCelsius"]
+        self.temperature_datapoints.append(temperature)
+
+        humidity = response.json()["devices"][0]["traits"]["sdm.devices.traits.Humidity"]["ambientHumidityPercent"]
+        self.humidity_datapoints.append(humidity)
+
+        print(temperature, humidity)
+
+
 class PlotlyLiveServer:
 
     def __init__(self):
         """Initialise sensor and graph figure data"""
         self.sensor_data = DHTSensorData()
+
+        self.nest_data = NestAPIData()
         
         self.fig = plotly.tools.make_subplots(rows=1, cols=1)
         self.fig.update_layout(
@@ -67,24 +118,28 @@ class PlotlyLiveServer:
             "x": [], 
             "y": [],
             "name": "Bedroom", 
-            "type": "scatter"}, 1, 1)
+            "type": "scatter",
+            "mode": "markers"}, 1, 1)
 
         self.fig.append_trace({
             "x": [], 
             "y": [],
             "name": "Living room", 
-            "type": "scatter"}, 1, 1)
+            "type": "scatter",
+            "mode": "markers"}, 1, 1)
 
         self.fig.update_layout(legend_title_text="Location", showlegend=True)
 
     def get_new_reading(self):
 
         self.sensor_data.get_new_reading()
+        self.nest_data.get_nest_sensor_data()
 
         self.fig["data"][0]["x"] = self.sensor_data.timestamps
         self.fig["data"][0]["y"] = self.sensor_data.temperature_datapoints
 
-        print(self.fig)
+        self.fig["data"][1]["x"] = self.sensor_data.timestamps # Use same timestamp as sensor data
+        self.fig["data"][1]["y"] = self.nest_data.temperature_datapoints
 
         return self.fig
 
@@ -102,7 +157,7 @@ if __name__ == '__main__':
             dcc.Graph(id='live-update-graph'),
             dcc.Interval(
                 id='interval-component',
-                interval=10*1000, # in milliseconds
+                interval=30*1000, # in milliseconds
                 n_intervals=0
             )
         ])
