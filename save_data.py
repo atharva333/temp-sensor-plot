6f767f90be51aa24c99a5660357b46fc135abdad
Atharva Deshmukh <atharvad@gmail.com>
2023-02-25 22:15:31 +0000

    Add basic error handling

--- save_data.py
+++ save_data.py
@@ -16,21 +16,25 @@ def main():
     external_sensor = ExternalTemperatureDB()
 
     while True:
-        pi_start_time = time.perf_counter()
-        pi_sensor.get_new_reading()
-        print(f"Pi sensor time: {time.perf_counter() - pi_start_time}")
+        try:
+            pi_start_time = time.perf_counter()
+            pi_sensor.get_new_reading()
+            print(f"Pi sensor time: {time.perf_counter() - pi_start_time}")
 
-        nest_start_time = time.perf_counter()
-        nest_sensor.get_new_reading()
-        print(f"Nest sensor time: {time.perf_counter() - nest_start_time}")
+            nest_start_time = time.perf_counter()
+            nest_sensor.get_new_reading()
+            print(f"Nest sensor time: {time.perf_counter() - nest_start_time}")
 
-        ext_start_time = time.perf_counter()
-        external_sensor.get_new_reading()
-        print(f"External sensor time: {time.perf_counter() - ext_start_time}")
+            ext_start_time = time.perf_counter()
+            external_sensor.get_new_reading()
+            print(f"External sensor time: {time.perf_counter() - ext_start_time}")
 
-        print(f"Sleep for {SAVE_INTERVAL - (ext_start_time - pi_start_time)}")
-        print("")
-        time.sleep(SAVE_INTERVAL - (ext_start_time - pi_start_time))
+            print(f"Sleep for {SAVE_INTERVAL - (ext_start_time - pi_start_time)}")
+            print("")
+            time.sleep(SAVE_INTERVAL - (ext_start_time - pi_start_time))
+
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
